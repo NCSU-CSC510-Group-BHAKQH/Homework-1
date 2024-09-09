790ce65837ee50e2f4e6d1abcf1986978c8289e6
Braden Helmer <bradenhelmeraus@gmail.com>
2024-09-09 09:57:44 -0400

    Commmitting pylint changes

--- rand.py
+++ rand.py
@@ -1,10 +1,16 @@
+"""
+Helper 'random' methods for merge-sort impl in hw2_debugging.py.
+"""
 import subprocess
 
 
 def random_array(arr):
+    """
+    Generates and returns a random array to be sorted.
+    """
     shuffled_num = None
-    for i in range(len(arr)):
+    for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True)
+            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
