fdf17388c3b6da9b00b8af7589a5540a1fcda2b4
Braden Helmer <bradenhelmeraus@gmail.com>
2024-09-09 14:05:10 +0000

    Committing bandit changes

--- rand.py
+++ rand.py
@@ -1,7 +1,7 @@
 """
 Helper 'random' methods for merge-sort impl in hw2_debugging.py.
 """
-import subprocess
+import subprocess # nosec
 
 
 def random_array(arr):
@@ -11,6 +11,6 @@ def random_array(arr):
     shuffled_num = None
     for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["shuf", "-i1-20", "-n1"], capture_output=True, check=True)
+            ["/usr/bin/env", "shuf", "-i1-20", "-n1"], capture_output=True, check=True) # nosec
         arr[i] = int(shuffled_num.stdout)
     return arr
