3da1ae5347c84f072de0702d6a620155fcbc5d67
Aditya Karthikeyan <161865393+AdrYne01@users.noreply.github.com>
2024-09-09 18:54:21 +0000

    Final changes to badges and workflows

--- rand.py
+++ rand.py
@@ -11,6 +11,7 @@ def random_array(arr):
     shuffled_num = None
     for i, _ in enumerate(arr):
         shuffled_num = subprocess.run(
-            ["/usr/bin/env", "shuf", "-i1-20", "-n1"], capture_output=True, check=True)  # nosec
+            # nosec
+            ["/usr/bin/env", "shuf", "-i1-20", "-n1"], capture_output=True, check=True)
         arr[i] = int(shuffled_num.stdout)
     return arr
